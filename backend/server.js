55853f838bf5bf2ed67a20f19801674389d86151
SatyaVamsi-Polupureddy <p.s.vamsi2021@gmail.com>
2025-06-01 13:23:49 +0530

    corrected frontend url

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,14 @@ import achievementsRoutes from './routes/achievements.js';
 dotenv.config();
 
 const app = express();
-const frontendUrl ='https://vamsi-portfolio-rust.vercel.app';
-
 // Middleware
 const corsOptions = {
-  origin: 'https://vamsi-portfolio-rust.vercel.app', // Replace with your frontend's URL
+  origin: 'https://vamsi-portfolio-rust.vercel.app/', // Replace with your frontend's URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // If you need to send cookies or authentication headers
 };
+
 // Apply CORS middleware globally
 app.use(cors(corsOptions));
 app.use(express.json());
